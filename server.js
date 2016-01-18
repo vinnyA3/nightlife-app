1d0ea12a57ba97ee9619f9e66410a388b5a7627a
vinnya3 <vinaceto@yahoo.com>
2016-01-18 11:03:05 -0500

    added additional CORS configuration

--- server.js
+++ server.js
@@ -21,8 +21,8 @@ app.use(bodyParser.json());
 //handle CORS requests
 app.use(function(req,res,next){
    res.setHeader('Access-Control-Allow-Origin', '*');
-   res.setHeader('Access-Control-Allow-Methods', 'GET,POST');
-   res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With, content-type, \Authorization');
+   res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, PATCH, OPTIONS');
+   res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With, content-type');
    next();
 });
 
