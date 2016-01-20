d753927da03e68d6bbd8f31e82fb0ae0eb139b33
vinnya3 <vinaceto@yahoo.com>
2016-01-19 23:37:34 -0500

    added remote db

--- config/config.js
+++ config/config.js
@@ -1,4 +1,5 @@
 module.exports = {
-  'db': 'mongodb://127.0.0.1:27017/nightlife_db',
+  //'db': 'mongodb://127.0.0.1:27017/nightlife_db',
+  'db' : 'mongodb://ace:qwertyasdf@ds047095.mongolab.com:47095/nightlife_db', //remote    
   'secret' : 'theregoesamanwhooncehaditall'
 };
