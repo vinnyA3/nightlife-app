b9be78c9404a372ddfa5ccc573fddefeb5ab42dd
vinnya3 <vinaceto@yahoo.com>
2016-01-31 16:27:59 -0500

    added facebook secret

--- config/config.js
+++ config/config.js
@@ -1,5 +1,10 @@
 module.exports = {
   //'db': 'mongodb://127.0.0.1:27017/nightlife_db',
   'db' : 'mongodb://ace:qwertyasdf@ds047095.mongolab.com:47095/nightlife_db', //remote    
-  'secret' : 'theregoesamanwhooncehaditall'
+  'secret' : 'theregoesamanwhooncehaditall',
+  //OAuth 2.0
+  FACEBOOK_SECRET: process.env.FACEBOOK_SECRET || 'e8388c27965b3cf6dfbcba27b76cd73f',
+  // OAuth 1.0
+  TWITTER_KEY: process.env.TWITTER_KEY || 'YOUR_TWITTER_CONSUMER_KEY',
+  TWITTER_SECRET: process.env.TWITTER_SECRET || 'YOUR_TWITTER_CONSUMER_SECRET'
 };
